12452481d01dfa6a857f1172f2b8d0ae9cbfb38b
Najaf Ali <ali.najaf@gmail.com>
2014-10-27 15:05:45 +0000

    Fetches json from S3 instead of running checks itself

--- app.rb
+++ app.rb
@@ -1,63 +1,23 @@
 require 'sinatra'
-require 'responsechecker'
-
-SITES = [
-  'https://www.prisonvisits.service.gov.uk/prisoner-details',
-  'https://people-finder.dsd.io/sessions/new',
-  'http://www.justice.gov.uk/',
-  'http://ajtc.justice.gov.uk/',
-  'http://azellerodneyinquiry.independent.gov.uk/',
-  'http://blogs.justice.gov.uk/',
-  'http://blogs.justice.gov.uk/cjg/',
-  'http://blogs.justice.gov.uk/digital/',
-  'http://blogs.justice.gov.uk/opgtransformation/',
-  'http://callmeback.justice.gov.uk/',
-  'http://cases.civillegaladvice.service.gov.uk/',
-  'http://civil-eligibility-calculator.justice.gov.uk/',
-  'http://cjsm.justice.gov.uk/',
-  'http://courttribunalfinder.service.gov.uk/',
-  'http://courttribunalformfinder.service.gov.uk/',
-  'http://dugganinquest.independent.gov.uk/',
-  'http://employmenttribunals.service.gov.uk/',
-  'http://find-legal-advice.justice.gov.uk/',
-  'http://globallawsummit.com/',
-  'http://hillsboroughinquests.independent.gov.uk/',
-  'http://jac.judiciary.gov.uk/',
-  'http://judicialconduct.judiciary.gov.uk/',
-  'http://lawcommission.justice.gov.uk/',
-  'http://legal-aid-checker.justice.gov.uk/',
-  'http://one3one.justice.gov.uk/',
-  'http://open.justice.gov.uk/',
-  'http://ppo.gov.uk/',
-  'http://probatesearch.service.gov.uk/',
-  'http://sentencingcouncil.judiciary.gov.uk/',
-  'http://trackparliamentaryquestions.service.gov.uk/',
-  'http://tribunalsdecisions.service.gov.uk/',
-  'http://victimservices.justice.gov.uk/',
-  'http://webchat.justice.gov.uk/',
-  'http://www.cjsm.justice.gov.uk/',
-  'http://www.employmenttribunals.service.gov.uk/',
-  'http://www.justice.gov.uk/',
-  'http://www.justice.gov.uk/about/imb',
-  'http://www.justice.gov.uk/youth-justice/publications',
-  'http://www.lastingpowerofattorney.service.gov.uk/',
-  'http://www.makeaplea.justice.gov.uk/',
-  'http://www.prisonvisits.service.gov.uk/',
-  'http://www.prisonvisits.service.gov.uk/prisoner-details',
-  'http://yjbdep.justice.gov.uk/',
-  'http://yjbpublications.justice.gov.uk/',
-  'http://yjbpublications.justice.gov.uk/en-gb/'
-]
+require 'open-uri'
+require 'json'
 
 def checks
-  SITES.shuffle.map do |url|
-    checker = ResponseChecker::Checker.new_with_all_checks(url)
-    begin
-      [url, checker.perform_checks]
-    rescue
-      # do nothing
+  f = open('https://s3-eu-west-1.amazonaws.com/opsreports/index.json')
+  JSON.parse(f.read).each.map do |url, data|
+    if data['response_checks']
+      check_data = data['response_checks']['checks'].map do |check_hash|
+        check_hash.values_at('name', 'description', 'result')
+      end
+      [url, check_data]
     end
-  end.compact.sort_by { |_, checks| checks.count { |_, _, passed| passed } }.reverse
+  end.compact
+end
+
+def sorted_checks
+  checks.sort_by do |url, check_data|
+    check_data.select { |_, _, result| !!result }.count
+  end
 end
 
 get '/' do
@@ -69,7 +29,7 @@ get '/' do
     'Strict transport security',
     'X-XSS-Protection'
   ]
-  @checks = checks
+  @checks = sorted_checks.reverse
   haml :index
 end
 
